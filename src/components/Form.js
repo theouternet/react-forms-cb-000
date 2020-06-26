0126e020645b262c809c58a4fdba175b57d4aee2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-26 00:09:31 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -41,10 +41,12 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+    <div>
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.lastName} />
+        </form>
+        {this.listOfSubmissions()}
+    </div>
     )
   }
 }
